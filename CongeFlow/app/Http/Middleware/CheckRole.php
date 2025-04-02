1e767eb478c4046908efa886a7e609f6007e1709
BouizmouneSalma <salmabouizmoun@gmail.com>
2025-04-02 16:02:06 +0000

    Modifiez le fichier pour handler les roles

--- CongeFlow/app/Http/Middleware/CheckRole.php
+++ CongeFlow/app/Http/Middleware/CheckRole.php
@@ -8,13 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CheckRole
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
+        if (!$request->user() || !in_array($request->user()->role, $roles)) {
+            return response()->json(['message' => 'Non autorise'], 403);
+        }
+
         return $next($request);
     }
 }
