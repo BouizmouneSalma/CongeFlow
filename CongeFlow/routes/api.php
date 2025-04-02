f5c51016694a04bed3ea3db166ce49bfca00bb84
BouizmouneSalma <salmabouizmoun@gmail.com>
2025-04-02 23:58:12 +0000

    Routes API

---
+++ CongeFlow/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Routes publiques
+Route::post('/login', [AuthController::class, 'login']);
+
+// Routes protégées
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+    
+});
