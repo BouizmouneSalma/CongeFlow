1875c6ceb7c18866c50055311be9f1bf11106844
BouizmouneSalma <salmabouizmoun@gmail.com>
2025-04-02 16:36:42 +0000

    Ajout de la fonction login

--- CongeFlow/app/Http/Controllers/AuthController.php
+++ CongeFlow/app/Http/Controllers/AuthController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'user' => $user,
+                'token' => $token,
+                'message' => 'Connexion réussie'
+            ]);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['Les informations d\'identification fournies sont incorrectes.'],
+        ]);
+    }
 }
