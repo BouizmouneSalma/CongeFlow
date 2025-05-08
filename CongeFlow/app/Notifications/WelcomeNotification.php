d02236f6ac86ca56b752d90b7d5a55fd7d037f8e
BouizmouneSalma <salmabouizmoun@gmail.com>
2025-05-08 21:56:23 +0100

    .

--- CongeFlow/app/Notifications/WelcomeNotification.php
+++ CongeFlow/app/Notifications/WelcomeNotification.php
@@ -10,13 +10,13 @@ use Illuminate\Notifications\Notification;
 class WelcomeNotification extends Notification implements ShouldQueue
 {
     use Queueable;
-
-    protected string $password;
+    
+    protected $password;
 
     /**
      * Create a new notification instance.
      */
-    public function __construct(string $password)
+    public function __construct($password)
     {
         $this->password = $password;
     }
@@ -24,9 +24,10 @@ class WelcomeNotification extends Notification implements ShouldQueue
     /**
      * Get the notification's delivery channels.
      *
-     * @return array<int, string>
+     * @param  mixed  $notifiable
+     * @return array
      */
-    public function via(object $notifiable): array
+    public function via($notifiable)
     {
         return ['mail'];
     }
@@ -34,29 +35,30 @@ class WelcomeNotification extends Notification implements ShouldQueue
     /**
      * Get the mail representation of the notification.
      */
-    public function toMail(object $notifiable): MailMessage
+    public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject('Bienvenue chez CongeFlow')
-            ->greeting('Bonjour ' . $notifiable->prenom . ' ' . $notifiable->nom . ' !')
-            ->line('Votre compte a été créé avec succès dans notre système de gestion.')
-            ->line('Voici vos informations de connexion:')
-            ->line('Email: ' . $notifiable->email)
-            ->line('Mot de passe: ' . $this->password)
-            ->action('Se connecter', url('/login'))
-            ->line('Pour des raisons de sécurité, nous vous recommandons de changer votre mot de passe après votre première connexion.')
-            ->line('Merci de faire partie de notre équipe !');
+                    ->subject('Bienvenue sur CongeFlow - Vos identifiants de connexion')
+                    ->greeting('Bonjour ' . $notifiable->prenom . ' ' . $notifiable->nom . ',')
+                    ->line('Bienvenue sur CongeFlow, votre nouvel outil de gestion des congés.')
+                    ->line('Un compte a été créé pour vous. Voici vos identifiants de connexion:')
+                    ->line('Email: ' . $notifiable->email)
+                    ->line('Mot de passe: ' . $this->password)
+                    ->action('Se connecter', url('/login'))
+                    ->line('Nous vous recommandons de changer votre mot de passe lors de votre première connexion.')
+                    ->line('Merci d\'utiliser notre application !');
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @return array<string, mixed>
+     * @param  mixed  $notifiable
+     * @return array
      */
-    public function toArray(object $notifiable): array
+    public function toArray($notifiable)
     {
         return [
-            //
+            'message' => 'Bienvenue sur CongeFlow !',
         ];
     }
 } 
